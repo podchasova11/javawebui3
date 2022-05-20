55f6fe606f5b91f971c5d6f535cce24dca559dc5
Mila <Pod4asova20@gmail.com>
2022-05-20 22:55:57 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/App.java
@@ -0,0 +1,9 @@
+package com.geekbrains;
+
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/geekbrains/AppTest.java
@@ -0,0 +1,13 @@
+package com.geekbrains;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
